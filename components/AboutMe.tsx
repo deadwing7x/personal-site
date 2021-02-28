3357c334ef7b7a2fe326443ea2be99ab637edf80
Anubhav Sarkar <anubhavsarkar7@gmail.com>
2021-02-28 17:16:46 +0530

    Removed images from skills

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,6 +1,19 @@
 import styles from "../styles/AboutMe.module.css";
 
 const AboutMe = () => {
+  const languages: string[] = [
+    "HTML & CSS",
+    "JavaScript (ES6+)",
+    "TypeScript",
+    "C#",
+    "React",
+    "Tailwind CSS",
+    "Bootstrap",
+    "Azure",
+    "SQL Server",
+    "Git",
+  ];
+
   return (
     <section
       className={`lg:mt-20 lg:ml-24 lg:mr-24 sm:ml-20 sm:mr-16 sm:mt-14 ${styles.aboutMeDiv}`}
@@ -68,163 +81,16 @@ const AboutMe = () => {
       <p className="text-gray-50 pt-12 lg:text-3xl sm:text-xl">
         Some languages and tools that I use:
       </p>
-      <div className={styles.skills}>
-        <ul className={`mt-5 ${styles.languages}`}>
-          <li>
-            <a
-              href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
-                alt="javascript"
-                width="40"
-                height="40"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.typescriptlang.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg"
-                alt="typescript"
-                width="40"
-                height="40"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://reactjs.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
-                alt="react"
-                width="40"
-                height="40"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://nextjs.org/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://cdn.worldvectorlogo.com/logos/nextjs-3.svg"
-                alt="nextjs"
-                width="40"
-                height="40"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.w3schools.com/cs/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/csharp/csharp-original.svg"
-                alt="csharp"
-                width="40"
-                height="40"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.w3.org/html/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
-                alt="html5"
-                width="40"
-                height="40"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.w3schools.com/css/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
-                alt="css3"
-                width="40"
-                height="40"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://dotnet.microsoft.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/dot-net/dot-net-original-wordmark.svg"
-                alt="dotnet"
-                width="40"
-                height="40"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://getbootstrap.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://raw.githubusercontent.com/devicons/devicon/master/icons/bootstrap/bootstrap-plain-wordmark.svg"
-                alt="bootstrap"
-                width="40"
-                height="40"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://azure.microsoft.com/en-in/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://www.vectorlogo.zone/logos/microsoft_azure/microsoft_azure-icon.svg"
-                alt="azure"
-                width="40"
-                height="40"
-              />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://git-scm.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <img
-                src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
-                alt="git"
-                width="40"
-                height="40"
-              />
-            </a>
-          </li>
-        </ul>
+      <div
+        className={`mt-5 grid lg:grid-cols-4 md:grid-cols-3 text-gray-50 lg:text-xl md:text-lg sm:text-md ${styles.languages}`}
+      >
+        {languages.map((item, i) => {
+          return (
+            <div key={i} className="w-max">
+              {item}
+            </div>
+          );
+        })}
       </div>
     </section>
   );
