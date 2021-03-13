f014fa3649164824653fda202a94a677863e8c04
Anubhav Sarkar <anubhavsarkar7@gmail.com>
2021-03-13 23:06:42 +0530

    Styling for footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,15 +1,21 @@
+import { CSSProperties } from "react";
 import { FaHeart } from "react-icons/fa";
 
 const Footer: React.FC<{}> = () => {
   const date: Date = new Date();
   const year = date.getFullYear();
 
+  const heartStyle: CSSProperties = {
+    display: "initial",
+    color: "red",
+  };
+
   return (
     <div className="mt-10 pb-5 text-center text-gray-50">
       <div className="copyright-text">
         Copyright © {year}
         <p>
-          Designed with <FaHeart style={{ fontSize: "1.5rem" }} /> by Anubhav
+          Designed with <FaHeart style={heartStyle} /> by Anubhav
         </p>
       </div>
     </div>
